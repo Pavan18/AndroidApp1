7440b76aece4a7cdcd70d1779ef33ee11f230774
Pavan18 <pavan18@gmail.com>
2016-01-31 13:36:54 -0800

    commit

---
+++ facebookpost/app/src/main/java/com/mobile/pavan/facebookpost/MainActivity.java
@@ -0,0 +1,68 @@
+package com.mobile.pavan.facebookpost;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import static java.lang.System.currentTimeMillis;
+
+public class MainActivity extends ActionBarActivity {
+    private ImageView img;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img=(ImageView)findViewById(R.id.imageView);
+        Button b1=(Button)findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                Uri screenshotUri = Uri.parse("android.resource://com.mobile.pavan.facebookpost");
+
+                try {
+                    InputStream stream = getContentResolver().openInputStream(screenshotUri);
+                }
+
+                catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                sharingIntent.setType("image/jpeg");
+                sharingIntent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
+                startActivity(Intent.createChooser(sharingIntent, "Share image using"));
+            }
+        });
+    }
+
+
+}
